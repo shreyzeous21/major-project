c7ddcfc9fe6b9a1b29971c20a0cd4b49ce0d5ce3
Zeous_Shrey <79036238+shreyzeous21@users.noreply.github.com>
2023-05-25 21:43:58 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const server = require('http').createServer(app);
 const PORT = 5001;
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://chat-app-mern-major.netlify.app/',
     methods: ['GET', 'POST']
   }
 })
